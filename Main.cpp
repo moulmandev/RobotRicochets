a3c2512b4d4b5b90435e91ae356f142fc1d2dc99
Mathilde <mathilde.marza@etu.univ-lyon1.fr>
2021-02-10 15:20:23 +0100

    Continuation fenetre Wt

--- Main.cpp
+++ Main.cpp
@@ -30,7 +30,14 @@
 #include <Wt/WApplication.h>
 #include <Wt/WStandardItemModel.h>
 #include <Wt/WTableView.h>
-
+#include <Wt/WAbstractItemModel.h>
+#include <Wt/WColor.h>
+#include <Wt/WCssDecorationStyle.h>
+#include <Wt/WFont.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WAbstractItemModel.h>
+#include <Wt/WStandardItemModel.h>
+#include <Windows.h>
 #include <cmath>
 
 
@@ -45,28 +52,66 @@ class HelloApplication : public Wt::WApplication
 public:
 	HelloApplication(const Wt::WEnvironment& env);
 
-	void fct() {
+	void menuPrincipalAlgorithmeResolution() {
 
-		/*auto tableView = cpp14::make_unique<WTableView>();
-		tableView->setModel(csvToModel(WApplication::appRoot() + "table.csv"));
+		setTitle("Resolution algorithme");
+		HWND hd = GetDesktopWindow();
+		RECT rect;
+		
+		GetWindowRect(hd, &rect);
+		int window_width = (rect.right - rect.left);
+		int window_height = (rect.bottom - rect.top);
 
-		tableView->setColumnResizeEnabled(false);
-		tableView->setColumnAlignment(0, AlignmentFlag::Center);
-		tableView->setHeaderAlignment(0, AlignmentFlag::Center);
-		tableView->setAlternatingRowColors(true);
-		tableView->setRowHeight(28);
-		tableView->setHeaderHeight(28);
-		tableView->setSelectionMode(SelectionMode::Single);
-		tableView->setEditTriggers(EditTrigger::None);*/
+		root()->resize(window_width, window_height);//Mettre taille ecran
+		Wt::WCssDecorationStyle decorationPage;
+		decorationPage.setBackgroundColor(Wt::WColor::WColor(135, 233, 144, 255));
+		//decorationPage.setBackgroundImage("C://Users//33660//Desktop//technology.jpg");
+
+		root()->setDecorationStyle(decorationPage);
+		Wt::WText* pageTitle = root()->addWidget(std::make_unique<Wt::WText>("Resolution d'algorithme"));
+		Wt::WFont fontTitle;
+		//fontTitle.setFamily(FontFamily::Monospace, "'Courier New'");
+		fontTitle.setSize(20);
+		pageTitle->decorationStyle().setFont(fontTitle);
 
 
-		/*cptClick++;
-		table->setWidth(Wt::WLength("100%"));
+		Wt::WText* ExplanationsPage = root()->addWidget(std::make_unique<Wt::WText>("Choisissez l'action que vous souhaitez effectuer : "));
+		Wt::WFont fontExplanations;
+		//fontExplanations.setFamily(FontFamily::Monospace, "'Courier New'");
+		fontExplanations.setSize(12);
+		ExplanationsPage->decorationStyle().setFont(fontTitle);
+
+		Wt::WPushButton* btnPrecomputeMinMoves = root()->addWidget(std::make_unique<Wt::WPushButton>("Precompute minimum moves"));
+		Wt::WPushButton* btnSolutionPath = root()->addWidget(std::make_unique<Wt::WPushButton>("Shortest path"));
+
+		btnSolutionPath->decorationStyle().setBackgroundColor(Wt::WColor::WColor(220, 220, 220, 155));
+		btnPrecomputeMinMoves->decorationStyle().setBackgroundColor(Wt::WColor::WColor(220, 220, 220, 155));
+
+
+		auto precomputeMinimumMovesArray = [this] {
+			fctDisplayPrecomputeMinimumMoves();
+		};
+
+		auto pathArray = [this] {
+			fctDisplayPath();
+		};
+
+		btnPrecomputeMinMoves->clicked().connect(precomputeMinimumMovesArray);
+		btnSolutionPath->clicked().connect(pathArray);
+
+	}
+
+	
+	void fctDisplayPrecomputeMinimumMoves() {
+		
+		Wt::WCssDecorationStyle PrecomputeMinimumArrayStyle;
+		PrecomputeMinimumArrayStyle.setBackgroundColor(Wt::WColor::WColor(220, 220, 220, 155));
+
+		table->setDecorationStyle(PrecomputeMinimumArrayStyle);
 
 		for (int i = 0; i < 16; i++) {
-			for (int j = 0; j < 16; j++) {
-				table->elementAt(i, j)->setColumnSpan());
-					if (grille->getPrecomputeMinimumMovesArray()[i * 16 + j] != INT_MAX && grille->getPrecomputeMinimumMovesArray()[i * 16 + j] != 0) {
+			for (int j = 0; j < 16; j++) {				
+				if (grille->getPrecomputeMinimumMovesArray()[i * 16 + j] != INT_MAX && grille->getPrecomputeMinimumMovesArray()[i * 16 + j] != 0) {
 					table->elementAt(i, j)->addNew<Wt::WText>(to_string(grille->getPrecomputeMinimumMovesArray()[i * 16 + j]));
 				}
 				else if (grille->getPrecomputeMinimumMovesArray()[i * 16 + j] ==0) {
@@ -76,17 +121,53 @@ public:
 					table->elementAt(i, j)->addNew<Wt::WText>("");
 				}
 			}
-		}*/
+		}
+	
 	}
 
+	
+
+	void fctDisplayPath() {
+		root()->refresh();///////////TROUVER AUTRE
+
+		vector <char> path;
+		path.push_back('t');
+		path.push_back('d');
+		path.push_back('l');
+		path.push_back('r');
+
+		Wt::WCssDecorationStyle textTop;
+		Wt::WCssDecorationStyle textDown;
+		Wt::WCssDecorationStyle textLeft;
+		Wt::WCssDecorationStyle textRight;
+
+		textTop.setForegroundColor(Wt::WColor(255, 140, 0, 155));
+		textDown.setForegroundColor(Wt::WColor(0, 255, 0, 155));
+		textLeft.setForegroundColor(Wt::WColor(255, 0, 0, 155));
+		textRight.setForegroundColor(Wt::WColor(0, 0, 255, 155));
+
+		
+		for (int i = 0; i < path.size(); i++){
+			cout << "On rentre " << endl;
+			if (path.at(i) == 't') {
+				root()->addWidget(std::make_unique<Wt::WText>("Top\n"))->setDecorationStyle(textTop);
+			}
+			else if (path.at(i) == 'd') {
+				root()->addWidget(std::make_unique<Wt::WText>("Down\n"))->setDecorationStyle(textDown);
+			}
+			else if (path.at(i) == 'l') {
+				root()->addWidget(std::make_unique<Wt::WText>("Left\n"))->setDecorationStyle(textLeft);
+			}
+			else if (path.at(i) == 'r') {
+				root()->addWidget(std::make_unique<Wt::WText>("Right\n"))->setDecorationStyle(textRight);
+			}
+		}
+	}
 
 private:
-	Wt::WLineEdit* nameEdit;
-	Wt::WText* greeting;
+	
 	Wt::WTable* table = root()->addWidget(std::make_unique<Wt::WTable>());
-	Wt::WTableView* table2 = root()->addWidget(std::make_unique<Wt::WTableView>());
-
-	int cptClick = 0;
+	//Wt::WTableView* table2 = root()->addWidget(std::make_unique<Wt::WTableView>());
 
 };
 
@@ -94,20 +175,8 @@ private:
 HelloApplication::HelloApplication(const Wt::WEnvironment& env)
 	: Wt::WApplication(env)
 {
-	setTitle("Resolution algorithme");
+	menuPrincipalAlgorithmeResolution();
 
-	
-
-	Wt::WPushButton* btnPrecomputeMinMoves = root()->addWidget(std::make_unique<Wt::WPushButton>("Precompute minimum moves"));
-	Wt::WPushButton* btnSolutionPath = root()->addWidget(std::make_unique<Wt::WPushButton>("Shortest path"));
-
-	
-	auto precomputeMinimumMovesArray = [this] {
-		fct();
-	};
-
-	btnPrecomputeMinMoves->clicked().connect(precomputeMinimumMovesArray);
-	btnSolutionPath->clicked().connect("Path le plus court");
 }
 
 
